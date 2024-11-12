86e0a7035401f07f4cad83c0556ea893cd928294
YuyingCYY <120092424+YuyingCYY@users.noreply.github.com>
2024-11-12 09:13:52 +0800

    加入專案檔案。

---
+++ FolderWatcher/Program.cs
@@ -0,0 +1,42 @@
+﻿// 監聽的資料夾路徑
+string folderPath = @"D:\Test";
+
+using (var watcher = new FileSystemWatcher(folderPath))
+{
+    // 訂閱檔案變更事件
+    watcher.Created += Watcher_Created;
+    watcher.Changed += Watcher_Changed;
+    watcher.Deleted += Watcher_Deleted;
+    watcher.Renamed += Watcher_Renamed;
+
+    // 設置監聽選項
+    watcher.EnableRaisingEvents = true;
+    watcher.IncludeSubdirectories = false;
+    watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+
+    // 持續監聽
+    while (true)
+    {
+        Thread.Sleep(1000);
+    }
+}
+
+void Watcher_Created(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"New file created: {e.FullPath}");
+}
+
+void Watcher_Deleted(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"File deleted: {e.FullPath}");
+}
+
+void Watcher_Changed(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"File changed: {e.FullPath}");
+}
+
+void Watcher_Renamed(object sender, RenamedEventArgs e)
+{
+    Console.WriteLine($"File renamed: {e.OldFullPath} -> {e.FullPath}");
+}
